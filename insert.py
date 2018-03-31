3568c14e36f3433a13f33231fa96589ad3d38a8b
EC2 Default User <ec2-user@ip-172-31-24-172.us-east-2.compute.internal>
2018-03-31 19:56:49 +0000

    Update

--- insert.py
+++ insert.py
@@ -4,69 +4,55 @@ import json
 import decimal
 from boto3.dynamodb.conditions import Key, Attr
 
-def insert():
-    class DecimalEncoder(json.JSONEncoder):
-        def default(self, o):
-            if isinstance(o, decimal.Decimal):
-                if o % 1 > 0:
-                    return float(o)
-                else:
-                    return int(o)
-    return super(DecimalEncoder, self).default(o)
-
-dynamodb = boto3.resource('dynamodb', region_name='us-west-1')
-
-table = dynamodb.Table('Swipes') # table 
-
-
-'''
-response = table.put_item(
-   Item={
-        'Location': "totalCount",
-        'totalNum': 1
-    }
-)
-'''
-
-
-
-response = table.update_item(
-    Key={
-        'Location': "totalCount",
-    },
-    UpdateExpression="set totalNum = totalNum + :val",
-    ExpressionAttributeValues={
-        ':val': 1
-    },
-    ReturnValues="UPDATED_NEW"
-)
-
-numItems = 0;
-
-response = table.query(
-    KeyConditionExpression=Key('Location').eq("totalCount") 
-)
-
-for i in response['Items']:
-    numItems = (i['totalNum'])
-
-convertedNumItems = str(numItems)   
-
-
-response = table.put_item(
-   Item={
-        'Location': "Bplato" + convertedNumItems,
-        'info': {
-            'price': 5,
-            'quantity': 3,
-            'timee': "3-4"
+def insert(m_key, m_price , m_quantity, m_timee , m_location):
+    
+    dynamodb = boto3.resource('dynamodb', region_name='us-west-1')
+    table = dynamodb.Table('Swipes') # table 
+    
+    
+    #updates totalNumber of items
+    response = table.update_item(
+        Key={
+            'Location': "totalCount",
+        },
+        UpdateExpression="set totalNum = totalNum + :val",
+        ExpressionAttributeValues={
+            ':val': 1
+        },
+        ReturnValues="UPDATED_NEW"
+    )
+    
+    numItems = 0
+    #get the current count of items
+    response = table.query(
+        KeyConditionExpression=Key('Location').eq('totalCount') 
+    )
+    
+    for i in response['Items']:
+        numItems = (i['totalNum'])
+    
+    convertedNumItems = str(numItems)   
+    
+    #inserting the new item with the new items
+    response = table.put_item(
+       Item={
+            'Location': m_location + convertedNumItems,
+            'info': {
+                'price': m_price,
+                'quantity': m_quantity,
+                'timee': m_timee
+            }
         }
-    }
-)
+    )
+    
+    
+    
+    print("PutItem succeeded:")
+    #print(json.dumps(response, indent=4, cls=DecimalEncoder)) 
 
 
 
-print("PutItem succeeded:")
-#print(json.dumps(response, indent=4, cls=DecimalEncoder))
+def main():
+    insert("Bplato",5,1,"3-9","Bplato")
 
-
+main()
