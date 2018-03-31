56fd0cae0cb530ac7a1c13d70095f2a26dd50d7d
EC2 Default User <ec2-user@ip-172-31-24-172.us-east-2.compute.internal>
2018-03-31 15:04:07 +0000

    Updated Functions and Added New Functions

--- insert.py
+++ insert.py
@@ -3,32 +3,32 @@ import boto3
 import json
 import decimal
 
-# Helper class to convert a DynamoDB item to JSON.
-class DecimalEncoder(json.JSONEncoder):
-    def default(self, o):
-        if isinstance(o, decimal.Decimal):
-            if o % 1 > 0:
-                return float(o)
-            else:
-                return int(o)
-        return super(DecimalEncoder, self).default(o)
+def insert():
+    class DecimalEncoder(json.JSONEncoder):
+        def default(self, o):
+            if isinstance(o, decimal.Decimal):
+                if o % 1 > 0:
+                    return float(o)
+                else:
+                    return int(o)
+    return super(DecimalEncoder, self).default(o)
 
 dynamodb = boto3.resource('dynamodb', region_name='us-west-1')
 
 table = dynamodb.Table('Swipes')
 
-diningHall = "Bplato"
-
 response = table.put_item(
    Item={
-        'Location': diningHall,
+        'Location': "location",
         'info': {
             'price': 4,
             'quantity': 3,
-            'Time': "3-4"
+            'timee': "3-4"
         }
     }
 )
 
 print("PutItem succeeded:")
 print(json.dumps(response, indent=4, cls=DecimalEncoder))
+
+
