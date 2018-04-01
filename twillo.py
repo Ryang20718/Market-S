6b7d3ee7805e7e74b7aa42fc3cbb6bfbe1e79e37
EC2 Default User <ec2-user@ip-172-31-24-172.us-east-2.compute.internal>
2018-04-01 00:05:20 +0000

    Added Twilio SMS MSG

---
+++ twillo.py
@@ -0,0 +1,14 @@
+# /usr/bin/env python
+# Download the twilio-python library from twilio.com/docs/libraries/python
+from twilio.rest import Client
+
+# Find these values at https://twilio.com/user/account
+account_sid = "AC94e44335f7486bdfba3be0cff727eb23"
+auth_token = "204dc04075eaa9831526661843dbdbea"
+
+client = Client(account_sid, auth_token)
+
+client.api.account.messages.create(
+    to="+17609252538",
+    from_="+14427776565",
+    body="Hello there!")
