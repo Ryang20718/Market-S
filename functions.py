0ea11f73746c160d7b98323130e7f7e75fa17dc2
EC2 Default User <ec2-user@ip-172-31-24-172.us-east-2.compute.internal>
2018-04-01 17:39:13 +0000

    Update

--- functions.py
+++ functions.py
@@ -1,3 +1,8 @@
+# /usr/bin/env python
+# Download the twilio-python library from twilio.com/docs/libraries/python
+from twilio.rest import Client
+
+# Find these values at https://twilio.com/user/account
 from __future__ import print_function # Python 2/3 compatibility
 import boto3
 from botocore.exceptions import ClientError
@@ -187,6 +192,17 @@ def scan(m_key):
 
 
 
+def twilioPhone(buyerNum, sellerNum):
+    account_sid = "AC94e44335f7486bdfba3be0cff727eb23"
+    auth_token = "204dc04075eaa9831526661843dbdbea"
+    
+    client = Client(account_sid, auth_token)
+    
+    client.api.account.messages.create(
+        to="+17609252538",
+        from_="+14427776565",
+        body="Hi, Im your seller! Where would you like to meet? My phone number is: 14427776565 ")
+    return
 
 
 
